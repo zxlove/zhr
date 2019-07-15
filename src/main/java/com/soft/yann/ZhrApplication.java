90d31becb2f837573259908a241c7ebe406a8a02
zxlove <zy13540326801@163.com>
2019-07-15 21:27:21 +0800

    Init project

---
+++ src/main/java/com/soft/yann/ZhrApplication.java
@@ -0,0 +1,13 @@
+package com.soft.yann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhrApplication.class, args);
+    }
+
+}
